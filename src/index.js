efbe012b1f9fb04c9fa40693f83ab07c28c84cb5
Lubou Lukashevich <lukashevich.rs@gmail.com>
2022-07-26 23:23:23 +0300

    init: Algoritmic task 5

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 
-// You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let result = [];
+  if (matrix !== undefined) { 
+    for (let i = 0; i < matrix.length; i++) {
+    ( i % 2 === 0) ? result.push(...matrix[i]) : result.push(...matrix[i].reverse());
+  }
 }
+  return result;
+}
+
+
+const matrix = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
+console.log(module.exports(matrix));
